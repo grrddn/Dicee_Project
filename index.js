79fd9203ab522af962730cb688bb165fce44f695
Daniel Ortega <grrddn@hotmail.com>
2025-09-18 16:46:42 -0500

    ok

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ window.onload = function() {
 
     document.querySelector(".img1").setAttribute("src", "../Dicee+Challenge+-+Starting+Files/images/dice" + randomNumber1 + ".png");
     document.querySelector(".img2").setAttribute("src", "../Dicee+Challenge+-+Starting+Files/images/dice" + randomNumber2 + ".png");
+    let title = document.querySelector("h1");
+
+    if (randomNumber1 > randomNumber2) {
+        title.textContent = "🚩 Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+        title.textContent = "Player 2 Wins! 🚩";
+    } else {
+        title.textContent = "Draw! 🎲";
+    }
 };
